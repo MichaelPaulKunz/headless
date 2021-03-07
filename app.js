a7b2e506032eef9c02a47d4ebd65f0d554e0b805
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-03-06 19:23:04 -0600

    2 cites, links working

---
+++ app.js
@@ -0,0 +1,46 @@
+
+class Cited extends React.Component {
+  constructor(props) { //don't destructure in constructor
+    super(props);
+    this.state = {
+    };
+    this.url = `${this.props.cite.url}`;
+    console.log(this.url);
+  }
+  render() {
+    return (
+      <div>
+        <li><a href={this.url}>{this.props.cite.title}</a><i>{` by ${this.props.cite.author}`}</i></li>
+      </div>
+    );
+  }
+}
+
+const Citations = ({ cites }) => (
+  <ul>
+    {cites.map((cite, index) => <Cited cite={cite} index={index} />)}
+  </ul>
+);
+
+function App(){
+  const citeOne = {
+    title: 'Headless Chrome: an answer to server-side rendering JS sites',
+    author: 'Eric Bidelman',
+    url: 'https://developers.google.com/web/tools/puppeteer/articles/ssr'
+  }
+  const citeTwo = {
+    title:'Headless Chrome: DevOps Love It, So Do Hackers, Here’s Why',
+    author:'Dima Bekerman',
+    url : `https://www.imperva.com/blog/headless-chrome-devops-love-it-so-do-hackers-heres-why/`
+  }
+  const citations = [citeOne, citeTwo];
+  return(
+    <div>
+      <h1>Works Cited: </h1>
+      {/*create instance of GroceryList component*/}
+      <Citations cites={citations}/>
+    </div>
+  )
+};
+//This renders the PopulateList component to the app id
+ReactDOM.render(<App />, document.getElementById('app'));
